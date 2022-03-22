75430c8381ddbe161f04d500f70b1ae1da8cb0ab
santijati <102007567+santijati@users.noreply.github.com>
2022-03-22 14:35:23 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name':"Employee Family",
+    'summary':"This module is for manage employee's family",
+    'description':"Write the module description here",
+    'author':"i am a developer",
+    'website':"http://www.nurosoft.id",
+    'category':'hr',
+    'version':'13.0.1.0',
+    'depends':['base','hr'],
+    'data':['security/ir.model.access.csv',
+            'views/employee_family_view.xml',
+            'views/employee_family_menus.xml',
+            ],
+}
---
+++ models/employee_family.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class EmployeeFamily(models.Model):
+    _name = 'employee.family'
+    name = fields.Char('Name')
+    date = fields.Date('Date of birth')
+    family_relation = fields.Selection(selection=[('ayah','Ayah'),('ibu','Ibu'), ('suami','Suami'), ('istri','Istri'),('saudara','Saudara'),('anak','Anak')], string="Family Relation")
+    employee_id = fields.Many2one('hr.employee',string='Family member of', required=True)
+    
+    def compute_allowance(self):
+        view_id=self.env.ref('employee_family.employee_family_list_view')
+        return{
+            'type':'ir.actions.act_window',
+            'name':'Family member of %s'%self.employee_id.display_name,
+            'view_mode':'tree',
+            'res_model':'employee.family',
+            'view_id':view_id.id,
+            'res_id':self.id,
+            'domain':[('employee_id','=',self.employee_id.id)]
+            }
