596c9049450d97d8086bc3d5a7c8694a5e929231
santijati <santit@nurosoft.id>
2022-03-25 11:38:49 +0700

    on progress poin 2 WF kodecustomer

--- ns_realfood_kodeCustomer/models/ns_realfood_codecust.py
+++ ns_realfood_kodeCustomer/models/ns_realfood_codecust.py
@@ -15,21 +15,22 @@ class NsCustomer(models.Model):
     _inherit = 'res.partner'
     ns_type = fields.Many2one(
         'ns.code.customer', string='Customer Type', ondelete='restrict')
-    # ns_cust_id = fields.One2many(
-    #     'res.partner', 'ns_type', string="Customer Code")
+    ns_cust_id = fields.One2many(
+        'res.partner', 'ns_type', string="Customer Code")
     ns_code_type = fields.Char(compute="_codetype", string='Customer Code')
-    ns_code_manual = fields.Char('Customer Code')
+    ns_code_manual = fields.Char(string='Customer Code', default='')
 
     @api.onchange('ns_type')
     def _codetype(self):
+        # cari yang single di id brapa aja
         query = self.env['ns.code.customer'].search(
             [('ns_mode', '=', 'single')])
-        if self.ns_type.id == query.id:
-            self.ns_code_type = query.ns_code
+        # nama mode nya
+        q = self.env['ns.code.customer'].search(
+            [('id', '=', self.ns_type.id)])
+        qmode = q.ns_mode
+        qcode = q.ns_code
+        if qmode == 'single':
+            self.ns_code_type = qcode
         else:
-            self.ns_code_type = self.ns_cust_id
-
-    @api.onchange('ns_code_type')
-    def writeManual(self, id):
-        custobj = self.env['res.partner'].search([('id', '=', id)])
-        custobj.write({'ns_code_manual': self.ns_code_type})
+            self.ns_code_type = ''
