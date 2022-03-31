b85f6b11b2e8feaba27d86d41e0df5818417fd69
santijati <santit@nurosoft.id>
2022-03-31 16:48:57 +0700

    wf 3 done (v.2)

--- ns_realfood_hakakses/__manifest__.py
+++ ns_realfood_hakakses/__manifest__.py
@@ -5,9 +5,9 @@
     'author': "Nurosoft - Santi Dev",
     'website': "http://www.nurosoft.id",
     'category': 'sales',
-    'version': '1.0',
+    'version': '2.0',
     'depends': ['base', 'sale_management'],
-    'data': ['security/ir.model.access.csv',
-             'views/ns_hakakses_view.xml',
-             ],
+    'data': [
+        'views/ns_hakakses_view.xml',
+    ],
 }
--- ns_realfood_hakakses/models/ns_realfood_hakakses.py
+++ ns_realfood_hakakses/models/ns_realfood_hakakses.py
@@ -2,19 +2,14 @@
 from odoo import api, models, fields, exceptions
 
 
-class NsHakAkses(models.Model):
-    _name = 'ns.hakakses'
-
-
 class NsOrder(models.Model):
     _inherit = 'sale.order'
     paid = fields.Boolean('')
 
-    @api.model
     def action_confirm(self):
         self.paid = True
 
-        if not self.env.user.has_group('ns_hakakses.confirmso'):
+        if not self.env.user.has_group('ns_realfood_hakakses.confirmso'):
             raise exceptions.UserError('You dont have access to confirm SO')
 
         return super(NsOrder, self).action_confirm()
@@ -22,7 +17,7 @@ class NsOrder(models.Model):
     def action_cancel(self):
         self.paid = False
 
-        if not self.env.user.has_group('ns_hakakses.cancelso'):
+        if not self.env.user.has_group('ns_realfood_hakakses.cancelso'):
             if self.state == 'sale' or self.state == 'done' or self.state == 'cancel':
                 raise exceptions.UserError('You dont have access to cancel SO')
 
